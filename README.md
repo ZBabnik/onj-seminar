d98070e01968be0fd307ecc19d4105241da935de
prevozi <pilot.polet2@gmail.com>
2020-03-30 13:25:57 +0200

    Added word vector embeddings into pipeline, added pickling for faster testing

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 ## Set up 
 
-file requirements.txt holds all packages
+file requirements.txt holds all packages.
 
 pip install -r requirements.txt
+
+if you are having problems with torch installation use
+ [conda environment.](https://docs.conda.io/en/latest/miniconda.html)
+ 
+ Download [word vector embeddings](https://drive.google.com/drive/folders/1nc0FovOn5pqEVjjmW_Swikc66Nel1i6j?usp=sharing)
+ and put them into the embeddings folder.
+ 
+ ## How to use
+ 
+ #### pickling
+ After the first run all steps of preprocessing should have been saved
+ in pickle files in pickle folder. If you want a fresh preprocessing 
+ change use_pickled_data = True to False in main.py or delete pickle folder.
--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 from readXls import ReadXls
+from tagger import Tagger
+from pymagnitude import Magnitude
+import _pickle as pickle
+from os import path, mkdir
 
-xls = ReadXls("data/AllDiscussionData.xls")
-print(xls.get_column_with_name("Message"))
+use_pickled_data = True
+try:
+    mkdir("pickle")
+except:
+    pass
+
+if __name__ == "__main__":
+    xls = ReadXls("data/AllDiscussionData.xls")
+    messages = xls.get_column_with_name("Message")
+    print(messages)
+
+    if not path.exists("pickle/lemmas.pkl") or not use_pickled_data:
+        tagger = Tagger()
+        lemmas = list(map(lambda t: tagger.lemmatiser(" "+str(t))[1], messages))
+        if use_pickled_data:
+            pickle.dump(lemmas, open("pickle/lemmas.pkl", "wb"))
+    if use_pickled_data:
+        lemmas = pickle.load(open("pickle/lemmas.pkl", "rb"))
+    print(lemmas)
+
+    if not path.exists("pickle/wikiWV.pkl") or not use_pickled_data:
+        wv_embeddings = Magnitude("embeddings/wiki.sl.magnitude")
+        word_vec = list(map(lambda t: wv_embeddings.query(t), lemmas))
+        if use_pickled_data:
+            pickle.dump(word_vec, open("pickle/wikiWV.pkl", "wb"))
+    if use_pickled_data:
+        word_vec = pickle.load(open("pickle/wikiWV.pkl", "rb"))
+    print(word_vec)
+
+    if not path.exists("pickle/elmoWV.pkl") or not use_pickled_data:
+        wv_embeddings = Magnitude("embeddings/slovenian-elmo.weights.magnitude")
+        word_vec = list(map(lambda t: wv_embeddings.query(t), lemmas))
+        if use_pickled_data:
+            pickle.dump(word_vec, open("pickle/elmoWV.pkl", "wb"))
+    if use_pickled_data:
+        word_vec = pickle.load(open("pickle/elmoWV.pkl", "rb"))
+    print(word_vec)
