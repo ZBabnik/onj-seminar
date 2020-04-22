a34903872704fd9b3304b137dd5afb6bcac4e7c2
MiStr1 <miha.stravs96@gmail.com>
2020-04-22 08:08:31 +0200

    removed pickle since it doesn't work anymore

--- main.py
+++ main.py
@@ -10,13 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
-
-
-use_pickled_data = True
-try:
-    mkdir("pickle")
-except:
-    pass
+from sklearn.metrics import f1_score
 
 
 class Lemmatize:
@@ -28,19 +22,14 @@ class Lemmatize:
         return self
 
     def transform(self, X, y=None, *args, **kwargs):
-        if not path.exists("pickle/lemmas.pkl") or not use_pickled_data:
-            # remove all non alphanumeric characters
-            no_non_alphanumeric_chars = map(lambda t: sub(r'[^a-zA-Z0-9]+', ' ', str(t)), X)
-            # if a letter is repeated 3 times its most likely to emphasize the text so its shortened to 1 repetition
-            no_triple_chars = map(lambda t: sub(r'(\w)\1\1*', r'\1', str(t)), no_non_alphanumeric_chars)
-            # get lemmas change all lemmas to lower case
-            lemmas = map(lambda t: list(map(lambda u: u.lower(), self.lemmatizer.lemmatiser(t, tagging=True)[1])), no_triple_chars)
-            # if lemmas are empty mark it with _
-            lemmas = list(map(lambda t: ['_'] if (not t) else t, lemmas))
-            if use_pickled_data:
-                pickle.dump(lemmas, open("pickle/lemmas.pkl", "wb"))
-        if use_pickled_data:
-            lemmas = pickle.load(open("pickle/lemmas.pkl", "rb"))
+        # remove all non alphanumeric characters
+        no_non_alphanumeric_chars = map(lambda t: sub(r'[^a-zA-Z0-9]+', ' ', str(t)), X)
+        # if a letter is repeated 3 times its most likely to emphasize the text so its shortened to 1 repetition
+        no_triple_chars = map(lambda t: sub(r'(\w)\1\1*', r'\1', str(t)), no_non_alphanumeric_chars)
+        # get lemmas change all lemmas to lower case
+        lemmas = map(lambda t: list(map(lambda u: u.lower(), self.lemmatizer.lemmatiser(t, tagging=True)[1])), no_triple_chars)
+        # if lemmas are empty mark it with _
+        lemmas = list(map(lambda t: ['_'] if (not t) else t, lemmas))
         return lemmas
 
 
@@ -59,13 +48,8 @@ class Word2vecWiki:
         return self
 
     def transform(self, X, y=None, *args, **kwargs):
-        if not path.exists("pickle/wikiWV.pkl") or not use_pickled_data:
-            wv_embeddings = Magnitude("embeddings/wiki.sl.magnitude")
-            word_vec = list(map(lambda t: np.sum(wv_embeddings.query(t), axis=0), X))
-            if use_pickled_data:
-                pickle.dump(word_vec, open("pickle/wikiWV.pkl", "wb"))
-        if use_pickled_data:
-            word_vec = pickle.load(open("pickle/wikiWV.pkl", "rb"))
+        wv_embeddings = Magnitude("embeddings/wiki.sl.magnitude")
+        word_vec = list(map(lambda t: np.sum(wv_embeddings.query(t), axis=0), X))
         return word_vec
 
 
@@ -74,13 +58,8 @@ class Word2vecElmo:
         return self
 
     def transform(self, X, y=None, **kwargs):
-        if not path.exists("pickle/elmoWV.pkl") or not use_pickled_data:
-            wv_embeddings = Magnitude("embeddings/slovenian-elmo.weights.magnitude")
-            word_vec = list(map(lambda t: np.sum(wv_embeddings.query(t), axis=0), X))
-            if use_pickled_data:
-                pickle.dump(word_vec, open("pickle/elmoWV.pkl", "wb"))
-        if use_pickled_data:
-            word_vec = pickle.load(open("pickle/elmoWV.pkl", "rb"))
+        wv_embeddings = Magnitude("embeddings/slovenian-elmo.weights.magnitude")
+        word_vec = list(map(lambda t: np.sum(wv_embeddings.query(t), axis=0), X))
         return word_vec
 
 
@@ -106,12 +85,6 @@ if __name__ == "__main__":
     pipelanes = [pipelane_lr1, pipelane_lr2, pipelane_lr3]
     pipelanes_dict = ["BOW", "word2vecWiki", "word2vecElmo"]
 
-    for pipelane in pipelanes:
+    for i, pipelane in enumerate(pipelanes):
         pipelane.fit(X_train, y_train)
-
-    for i, model in enumerate(pipelanes):
-        scr = 0
-        for a,b in zip(y_test, model.predict(X_test)):
-            if a == b:
-                scr += 1
-        print("{} Test Accuracy: {}".format(pipelanes_dict[i], scr/len(y_test)))
+        print("{} Test Accuracy: {}".format(pipelanes_dict[i], pipelane.score(X_test, y_test)))
