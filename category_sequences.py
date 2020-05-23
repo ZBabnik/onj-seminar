6930efd977d377e45be0f6c38167d9c76bebb39f
Ziga Babnik <zb1996@student.uni-lj.si>
2020-05-23 19:20:45 +0200

    commented the stats scripts

--- category_sequences.py
+++ category_sequences.py
@@ -1,31 +1,31 @@
 
 import numpy as np
 import networkx as nx
-import matplotlib.pyplot as plt
 
 from readXls import ReadXls
 
-
-
-
+"""
+    Build Markov chains for data
+"""
 def get_sequences(data, prev):
 
+    # Create directed network
     G = nx.DiGraph()
 
+    # Create list of tuples of all sequences of prev + 1 length
     all_seq = []
     for i in range(len(data)):
         if len(data[i]) >= prev + 1:
             for j in range(prev, len(data[i])):
                 all_seq.append(("+".join([data[i][j - (prev - k)] for k in range(prev)]), data[i][j]))
 
-    #for x in all_seq:
-    #    print(x)
-
+    # Initialize all nodes
     for start, end in all_seq:
         G.add_node(start, nof_outedges=0)
         G.add_node(end, nof_outedges=0)
 
-    cummulative_weights = 0
+    # Create all edges
+    cummulative_weights = 0                             # Count number of all edges
     for start, end in all_seq:
         if G.has_edge(start, end):
             G[start][end]["label"] += 1
@@ -35,40 +35,60 @@ def get_sequences(data, prev):
             cummulative_weights += 1
         G.nodes[start]["nof_outedges"] += 1
 
+    # Find less frequent edges
     to_remove_edges = []
     for u, v, data in G.edges(data=True):
-        if data["label"] < (cummulative_weights * 0.005):
+        if data["label"] < (cummulative_weights * 0.01):
             to_remove_edges.append((u, v))
     G.remove_edges_from(to_remove_edges)
 
+    # Remove less frequent edges and isolates
     to_remove_nodes = nx.isolates(G)
     G.remove_nodes_from(list(to_remove_nodes))
 
+    # Normalize the edges weights
     for u, v, data in G.edges(data=True):
         data["label"] = round(float(data["label"]) / float(G.nodes[u]["nof_outedges"]), 3)
 
+    # Create .dot file
     nx.drawing.nx_pydot.write_dot(G, 'graph.dot')
 
+
 if __name__ == "__main__":
+
+    # Create excel table reader object
     xls = ReadXls("data/AllDiscussionData.xls")
+    # Read messages
     messages = np.array(xls.get_column_with_name("Message"))
-    relevance = np.array(list(filter(lambda t: t, xls.get_column_with_name("Book relevance"))))  # ground truth
-    type = np.array(list(filter(lambda t: t, xls.get_column_with_name("Type"))))  # ground truth
-    messages_gt = np.array(list(filter(lambda t: t, xls.get_column_with_name("Category"))))
-    ctg_brd = np.array(list(filter(lambda t: t, xls.get_column_with_name("CategoryBroad"))))
+    # Read relevance class
+    relevance = np.array(list(filter(lambda t: t, xls.get_column_with_name("Book relevance"))))
+    # Read type class
+    type = np.array(list(filter(lambda t: t, xls.get_column_with_name("Type"))))
+    # Read category class
+    category = np.array(list(filter(lambda t: t, xls.get_column_with_name("Category"))))
+    # Read category broad class
+    category_broad = np.array(list(filter(lambda t: t, xls.get_column_with_name("CategoryBroad"))))
+    # Read topic labels
     topic = np.array(list(filter(lambda t: t, xls.get_column_with_name("Topic"))))
+    # Read bookclub labels
     bookclub = np.array(list(filter(lambda t: t, xls.get_column_with_name("Bookclub"))))
+    # Read message time labels
     time = np.array(list(filter(lambda t: t, xls.get_column_with_name("Message Time"))))
 
+    # Create a joint label for topic, bookclub, time and category
     joint_data = []
-    for i, data in enumerate(zip(topic, bookclub, time, messages_gt)):
+    for i, data in enumerate(zip(topic, bookclub, time, category)):
+        # Addressing problems with two different time stamp structures
         if len(data[2].split(" ")) > 1:
             temp_date = data[2].split(" ")[0]
         else:
             temp_date = -1
         joint_data.append((data[0], data[1], data[2], i, temp_date, data[3]))
+
+    # Sort joint data by topic then bookclub then time and then index
     joint_data = sorted(joint_data, key=lambda x: (x[0], x[1], x[2], x[3]))
 
+    # Cut joint data by topic club and date
     currTopic, currClub, currDate = "", "", ""
     data_cut, temp = [], []
     for line in joint_data:
@@ -81,12 +101,8 @@ if __name__ == "__main__":
             currDate = line[4]
             temp = []
 
+    # Remove empty lines
     data_cut = list(filter(None, data_cut))
 
-    #for x in joint_data:
-    #    print(x)
-
-    #for x in data_cut:
-    #    print(x)
-
-    get_sequences(data_cut, 3)
+    # Get Markov chains from data
+    get_sequences(data_cut, 2)
--- class_dependency.py
+++ class_dependency.py
@@ -5,18 +5,19 @@ import matplotlib.pyplot as plt
 from readXls import ReadXls
 
 
-
+"""
+    Creates dictionary of label: int values
+"""
 def make_value_dict(data):
-
-    # Creates dictionary of label: int values
-
     value_dict = dict()
     for i, item in enumerate(sorted(set(data))):
         value_dict[item] = i
 
     return value_dict
 
-
+"""
+    Return color based on label
+"""
 def get_color(label):
 
     if label[0] == "C":
@@ -30,91 +31,132 @@ def get_color(label):
     else:
         return "y"
 
-
+"""
+    Plot the prior distribution
+"""
 def plot_prior(data, labels, name):
 
+    # Create figure
+    fig, ax = plt.subplots()
+
+    # X ticks list
     x = np.arange(len(labels))
 
+    # Get data and labels
     data, labels = zip(*list(sorted(zip(data, labels), key=lambda x: x[0], reverse=True)))
 
+    # Set colors for category_broad labels
     _colors = [get_color(label) for label in labels]
 
-    fig, ax = plt.subplots()
+    # Plot data
     ax.bar(x, data, color=_colors)
-    plt.title(name)
+
+    # Set x axis
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
+
+    # Set title
+    plt.title(name)
+
+    # Rotate labels
     plt.xticks(rotation=90)
+
+    # Show plot
     plt.show()
 
     return labels
 
-
+"""
+    Plot the posterior distribution
+"""
 def plot_posterior(data, labels1, labels2, name, prior_labels):
 
-    x = np.arange(len(labels2))
-
+    # Create figure
     fig, ax = plt.subplots()
+
+    # X ticks list
+    x = np.arange(len(labels2))
 
     # Remove labels with little data
     total_sum = sum([sum(x) for x in data])
     data, labels1 = zip(*[(x, y) for x, y in zip(data, labels1) if sum(x) > total_sum * 0.1])
 
-    print(prior_labels)
-    print(labels2)
-
+    # List for data reordering
     _transform = [labels2.index(label) for label in prior_labels]
 
-    print(_transform)
-
+    # Plot data
     for i in range(len(data)):
         ax.plot(x, [data[i][j] for j in _transform], label=labels1[i], marker="o")
 
+    # Set x ticks and labels
     ax.set_xticks(x)
     ax.set_xticklabels([labels2[j] for j in _transform])
+
+    # Set title and legend
     plt.title(name)
     plt.legend()
+
+    # Tilt text
     plt.xticks(rotation=90)
+
+    # Show plot
     fig.tight_layout()
     plt.show()
 
 
+"""
+    Returns order list of labels
+"""
 def get_labels(dict_val):
     labels = ["" for _, _ in dict_val.items()]
     for key, item in dict_val.items():
         labels[item] = key
     return labels
 
-
+"""
+    Plots prior/posterior distribution
+"""
 def compare_distributions(data1, data2, cls1_name, cls2_name):
 
+    # Build label: int dictionary for data1/data2
     cls1 = make_value_dict(data1)
     cls2 = make_value_dict(data2)
 
+    # Build list that will hold the results
     cls1_cls2_relation = [[0 for _, _ in cls2.items()] for _, _ in cls1.items()]
     cls2_relation = [0 for _, _ in cls2.items()]
+    # Iterate over all data labels
     for d1, d2 in zip(data1, data2):
+        # Count prior and posterior distributions
         cls1_cls2_relation[cls1[d1]][cls2[d2]] += 1
         cls2_relation[cls2[d2]] += 1
 
+    # Plot the prior distribution
     labels = plot_prior(cls2_relation, get_labels(cls2), str(cls2_name)+" distribution")
+    # Plot posterior distribution
     plot_posterior(cls1_cls2_relation, get_labels(cls1), get_labels(cls2), str(cls1_name)+"-"+str(cls2_name)+" distribution", labels)
 
-
-def join_data(relevance, type):
-    return [str(r)+"+"+str(l) for r, l in zip(relevance, type)]
+"""
+    Joins two labels into one
+"""
+def join_data(data1, data2):
+    return [str(r)+"+"+str(l) for r, l in zip(data1, data2)]
 
 
 if __name__ == "__main__":
-    xls = ReadXls("data/AllDiscussionData.xls")
-    messages = np.array(xls.get_column_with_name("Message"))
-    relevance = np.array(list(filter(lambda t: t, xls.get_column_with_name("Book relevance"))))  # ground truth
-    type = np.array(list(filter(lambda t: t, xls.get_column_with_name("Type"))))  # ground truth
-    messages_gt = np.array(list(filter(lambda t: t, xls.get_column_with_name("Category"))))
-    ctg_brd = np.array(list(filter(lambda t: t, xls.get_column_with_name("CategoryBroad"))))
 
-    #compare_distributions(relevance, ctg_brd, "Relevance", "CategoryBroad")
-    #compare_distributions(relevance, messages_gt, "Relevance", "Category")
-    #compare_distributions(type, ctg_brd, "Type", "CategoryBroad")
-    #compare_distributions(type, messages_gt, "Type", "Category")
-    compare_distributions(join_data(relevance, type), messages_gt, "Relevance+Type", "Category")
+    # Create excel table reader object
+    xls = ReadXls("data/AllDiscussionData.xls")
+    # Read messages
+    messages = np.array(xls.get_column_with_name("Message"))
+    # Read relevance class
+    relevance = np.array(list(filter(lambda t: t, xls.get_column_with_name("Book relevance"))))
+    # Read type class
+    type = np.array(list(filter(lambda t: t, xls.get_column_with_name("Type"))))
+    # Read category class
+    category = np.array(list(filter(lambda t: t, xls.get_column_with_name("Category"))))
+    # Read category broad class
+    category_broad = np.array(list(filter(lambda t: t, xls.get_column_with_name("CategoryBroad"))))
+
+    # Run category distribution comparison by first splitting the data by relevance+type
+    compare_distributions(join_data(relevance, type), category, "Relevance+Type", "Category")
